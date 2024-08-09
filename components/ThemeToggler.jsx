f034acdd1017caf00547ea888b25ae5730f8861c
S MUZAMMIL JAVED <s.muzammiljaved@gmail.com>
2024-08-09 08:40:34 +0500

    themes

--- components/ThemeToggler.jsx
+++ components/ThemeToggler.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+"use client";
 
-const ThemeToggler = () => {
+import * as React from "react";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+const ThemeToggler=()=> {
+  const { setTheme, themes } = useTheme();
+
   return (
-    <div>ThemeToggler</div>
-  )
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        {themes.map((theme) => (
+          <DropdownMenuItem key={theme} onClick={() => setTheme(theme)}>
+            <div>
+              <span className=""></span>
+              <span>{theme}</span>
+            </div>
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 }
-
 export default ThemeToggler
